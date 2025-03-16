287a7612a6a3975107dbfb0ad2821c63dd7148f1
KENETH WILLARD LĂ“PEZ OVALLE <3116549240704@ingenieria.usac.edu.gt>
2025-03-16 14:55:32 -0600

    Agregar soporte para el tipo de dato Slice y mejorar la validación de tipos en el analizador

--- client/Analyzer/Symbol.cs
+++ client/Analyzer/Symbol.cs
@@ -4,8 +4,9 @@ public enum ValueType
     Float,
     String,
     Bool,
-    Rune,  
-    Nil   
+    Rune,
+    Slice,
+    Nil
 }
 
 public class Value
@@ -15,15 +16,20 @@ public class Value
 
     public Value(ValueType type, object data)
     {
+        if (!ValidateType(type, data))
+            throw new Exception($"El tipo {data?.GetType()} no es válido para {type}.");
+
         Type = type;
         Data = data;
     }
 
+    // Accesores tipados
     public int AsInt() => (int)Data;
     public double AsFloat() => (double)Data;
     public bool AsBool() => (bool)Data;
     public string AsString() => (string)Data;
-    public char AsRune() => (char)Data;  // NUEVO para runes
+    public char AsRune() => (char)Data;
+    public Slice AsSlice() => (Slice)Data;
 
     // Creadores estáticos
     public static Value FromInt(int i)       => new Value(ValueType.Int, i);
@@ -31,6 +37,7 @@ public class Value
     public static Value FromString(string s) => new Value(ValueType.String, s);
     public static Value FromBool(bool b)     => new Value(ValueType.Bool, b);
     public static Value FromRune(char c)     => new Value(ValueType.Rune, c);
+    public static Value FromSlice(Slice s)   => new Value(ValueType.Slice, s);
     public static Value FromNil()            => new Value(ValueType.Nil, null);
 
     public static Value DefaultForType(ValueType t)
@@ -41,27 +48,65 @@ public class Value
             ValueType.Float  => FromFloat(0.0),
             ValueType.String => FromString(""),
             ValueType.Bool   => FromBool(false),
-            ValueType.Rune   => FromRune('\0'),     
+            ValueType.Rune   => FromRune('\0'),
+            ValueType.Slice  => FromSlice(new Slice(ValueType.Nil)),
             ValueType.Nil    => FromNil(),
-            _                => FromNil()          
+            _                => FromNil()
+        };
+    }
+    public override string ToString()
+    {
+        switch (Type)
+        {
+            case ValueType.Int:
+                return AsInt().ToString();
+            case ValueType.Float:
+                return AsFloat().ToString();
+            case ValueType.Bool:
+                return AsBool() ? "true" : "false";
+            case ValueType.String:
+                return AsString();
+            case ValueType.Rune:
+                return AsRune().ToString();
+            case ValueType.Slice:
+                {
+                    // Llamamos a Slice.ToString()
+                    Slice sliceData = AsSlice();
+                    return sliceData == null ? "[]" : sliceData.ToString();
+                }
+            case ValueType.Nil:
+            default:
+                return "nil";
+        }
+    }
+
+    private bool ValidateType(ValueType type, object data)
+    {
+        return type switch
+        {
+            ValueType.Int    => data is int,
+            ValueType.Float  => data is double,
+            ValueType.String => data is string,
+            ValueType.Bool   => data is bool,
+            ValueType.Rune   => data is char,
+            ValueType.Slice  => data is Slice,
+            ValueType.Nil    => data == null,
+            _                => false
         };
     }
 }
-
-
 
 public class Symbol
 {
     public string Name { get; set; }
     public ValueType DeclaredType { get; set; }
-    public Value RuntimeValue { get; set; } 
-
+    public Value RuntimeValue { get; set; }
 
     public Symbol(string name, ValueType declaredType)
     {
         Name = name;
         DeclaredType = declaredType;
-        RuntimeValue = new Value(declaredType, GetDefaultValue(declaredType));
+        RuntimeValue = Value.DefaultForType(declaredType);
     }
 
     public Symbol(string name, ValueType declaredType, Value initialValue)
@@ -71,15 +116,4 @@ public class Symbol
         RuntimeValue = initialValue;
     }
 
-    private object GetDefaultValue(ValueType t)
-    {
-        return t switch
-        {
-            ValueType.Int => 0,
-            ValueType.Float => 0.0,
-            ValueType.String => "",
-            ValueType.Bool => false,
-            _ => null!, 
-        };
-    }
 }
