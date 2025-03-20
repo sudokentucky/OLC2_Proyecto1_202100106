237917b51b5ca8229809a60f9c1833ebaa74b54f
KENETH WILLARD LĂ“PEZ OVALLE <3116549240704@ingenieria.usac.edu.gt>
2025-03-19 22:59:53 -0600

    Agregar métodos para manejar funciones en la clase StructType y actualizar la clase StructInstance para su uso

--- client/Analyzer/Visitors/StructType.cs
+++ client/Analyzer/Visitors/StructType.cs
@@ -1,7 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 public class StructType
 {
     public string Name { get; }
     public Dictionary<string, ValueType> Fields { get; } = new Dictionary<string, ValueType>();
+    
+    public Dictionary<string, Function> Methods { get; } = new Dictionary<string, Function>();
 
     public StructType(string name) => Name = name;
 
@@ -14,19 +20,40 @@ public class StructType
     }
 
     public bool HasField(string name) => Fields.ContainsKey(name);
+
+    public void AddMethod(string name, Function method)
+    {
+        if (Methods.ContainsKey(name))
+            throw new Exception($"El método '{name}' ya existe en el struct '{Name}'.");
+        
+        Methods.Add(name, method);
+        Console.WriteLine($"DEBUG: Método '{name}' agregado al struct '{Name}'");
+    }
+
+    public bool HasMethod(string name) => Methods.ContainsKey(name);
+
+    public Function GetMethod(string name)
+    {
+        if (!Methods.ContainsKey(name))
+            throw new Exception($"El método '{name}' no existe en el struct '{Name}'.");
+        
+        return Methods[name];
+    }
+    
+    public IEnumerable<string> GetMethodNames() => Methods.Keys;
 }
 
 public class StructInstance : Value
 {
     public string StructName { get; }
-    public StructType TypeDefinition { get; }
+    public StructType Type { get; }
     public Dictionary<string, Value> Fields { get; } = new Dictionary<string, Value>();
 
     public StructInstance(StructType structType)
         : base(ValueType.Struct, null)
     {
         StructName = structType.Name;
-        TypeDefinition = structType;
+        Type = structType;
 
         foreach (var field in structType.Fields)
         {
@@ -36,11 +63,11 @@ public class StructInstance : Value
 
     public void SetField(string name, Value value)
     {
-        if (!TypeDefinition.HasField(name))
+        if (!Type.HasField(name))
             throw new Exception($"El campo '{name}' no existe en el struct '{StructName}'.");
 
-        var expectedType = TypeDefinition.Fields[name];
-        if (expectedType != value.Type)
+        var expectedType = Type.Fields[name];
+        if (expectedType != value.Type && value.Type != ValueType.Nil)
             throw new Exception($"El campo '{name}' espera un valor de tipo '{expectedType}', se recibió '{value.Type}'.");
 
         Fields[name] = value;
@@ -53,10 +80,16 @@ public class StructInstance : Value
 
         return Fields[name];
     }
+    
+    public bool HasMethod(string name) => Type.HasMethod(name);
+    
+    public Function GetMethod(string name) => Type.GetMethod(name);
 
     public override string ToString()
     {
         var fieldValues = string.Join(", ", Fields.Select(f => $"{f.Key}: {f.Value}"));
         return $"{StructName} {{ {fieldValues} }}";
     }
+    
+
 }
