19931be16650cb9648ad67f4e4c179f5a7e27c31
KENETH WILLARD LĂ“PEZ OVALLE <3116549240704@ingenieria.usac.edu.gt>
2025-03-19 22:59:34 -0600

    Agregar clase AstNode con propiedades y métodos para manejar nodos en el árbol AST

---
+++ client/Analyzer/AST/ASTnode.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+public class AstNode
+{
+    public string Label { get; set; }             
+    public List<AstNode> Children { get; set; }   
+    public int Id { get; private set; }           
+
+    private static int counter = 0;
+
+    public AstNode(string label)
+    {
+        this.Label = label;
+        this.Children = new List<AstNode>();
+        this.Id = counter++;
+    }
+
+    public void AddChild(AstNode child)
+    {
+        this.Children.Add(child);
+    }
+}
